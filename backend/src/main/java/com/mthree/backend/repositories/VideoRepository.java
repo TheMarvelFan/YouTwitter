b8e991274fddbc119f9bd6d0b97ee4c6dc19169d
Kushagra Nigam <93257324+TheMarvelFan@users.noreply.github.com>
2025-07-07 18:55:51 +0530

    added findById method to fetch video by ID

--- backend/src/main/java/com/mthree/backend/repositories/VideoRepository.java
+++ backend/src/main/java/com/mthree/backend/repositories/VideoRepository.java
@@ -1,6 +1,7 @@
 package com.mthree.backend.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import lombok.NonNull;
 import org.springframework.data.domain.Page;
@@ -10,7 +11,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mthree.backend.models.User;
 import com.mthree.backend.models.Video;
 
-public interface VideoRepository extends JpaRepository<     Video, Long> {
+public interface VideoRepository extends JpaRepository<Video, Long> {
     @NonNull
     Page<Video> findAll(@NonNull Pageable pageable);
 
@@ -19,6 +20,9 @@ public interface VideoRepository extends JpaRepository<     Video, Long> {
     List<Video> findByOwner(User owner);
 
     Page<Video> findAllByOwner(User owner, Pageable pageable);
+
+    @NonNull
+    Optional<Video> findById(@NonNull Long id);
 
     Page<Video> findAllByisPublished(boolean is_published, Pageable pageable);
 }
